1bb5517afbd1124597ff530dae0f6e7027674a43
Clément L <clementlandais@gmail.com>
2023-04-20 18:39:19 +0200

    Initial Commit

---
+++ Core/Editor/Bounds2DEditor.cs
@@ -0,0 +1,23 @@
+﻿using Klem.Core.Utils;
+using UnityEditor;
+using UnityEngine;
+
+namespace Klem.Core
+{
+    [CustomEditor(typeof(Bounds2DComponent))]
+    public class Bounds2DEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var bounds2DComponent = (Bounds2DComponent) target;
+            
+            
+            if (GUILayout.Button("Calculate Bounds"))
+            {
+                bounds2DComponent.CalculateBounds();
+            }
+            
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Core/Runtime/Utils/ObservableVariable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Klem.Core.Utils
+{
+    /// <summary>
+    /// A simple object that contains a value and can be observed for changes.
+    /// </summary>
+    /// <typeparam name="T">The type of the observed value</typeparam>
+    [Serializable]
+    public class ObservableVariable<T>
+    {
+        /// <summary>
+        /// The current value.
+        /// </summary>
+        [SerializeField] private T value;
+
+        /// <summary>
+        /// The event that is invoked when the value changes.
+        /// </summary>
+        [NonSerialized] public UnityAction<T, T> OnValueChanged;
+
+        
+        /// <summary>
+        ///  The current value.
+        /// </summary>
+        public T Value
+        {
+            get => value;
+            set
+            {
+                var previous = this.value;
+                this.value = value;
+                OnValueChanged?.Invoke(previous, this.value);
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public ObservableVariable(T value)
+        {
+            this.value = value;
+        }
+
+        public ObservableVariable()
+        {
+
+        }
+    }
+}
