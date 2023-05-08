dc6d65a73e5da1ea6935b3c5bf5081a5f0b33257
Clément L <clementlandais@gmail.com>
2023-05-08 16:09:04 +0200

    Fixed an issues with miscalculated Bounds2D - I am stupid - Made it auto geberate bounds if unset

--- Runtime/Utils/Bounds2DComponent.cs
+++ Runtime/Utils/Bounds2DComponent.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 namespace Klem.Utils
 {
-    
     /// <summary>
-    /// This component is used to calculate the bounds of a sprite or a collider 2D.
+    ///     This component is used to calculate the bounds of a sprite or a collider 2D.
     /// </summary>
     [AddComponentMenu("Klem/Bounds2DComponent")]
     public class Bounds2DComponent : MonoBehaviour
@@ -16,55 +15,60 @@ namespace Klem.Utils
             Sprites,
             Colliders
         }
-        
+
         /// <summary>
-        /// Can be set manually or calculated with the button.
+        ///     Can be set manually or calculated with the button.
         /// </summary>
         [SerializeField] private Bounds bounds;
-        
+
         /// <summary>
-        /// The color of the gizmo to draw the bounding box
+        ///     The color of the gizmo to draw the bounding box
         /// </summary>
-        [SerializeField] private Color boundsGizmoColor = new Color(0.51f, 1f, 0f);
-        
+        [SerializeField] private Color boundsGizmoColor = new(0.51f, 1f, 0f);
+
         /// <summary>
-        ///  The type of the bounds to calculate.
+        ///     The type of the bounds to calculate.
         /// </summary>
         [SerializeField] private BoundsType boundsType = BoundsType.Sprites;
-        
+
         /// <summary>
-        ///  Returns the bounds for use in other scripts. (ex: <see cref="Physics2D.OverlapBox(UnityEngine.Vector2,UnityEngine.Vector2,float)"/>
+        ///     Returns the bounds for use in other scripts. (ex:
+        ///     <see cref="Physics2D.OverlapBox(UnityEngine.Vector2,UnityEngine.Vector2,float)" />
         /// </summary>
         public Bounds Bounds => bounds;
-        
-        
+
+
         /// <summary>
-        ///  Draws the bounds as a gizmo.
+        ///     Draws the bounds as a gizmo.
         /// </summary>
         private void OnDrawGizmos()
         {
             Gizmos.color = boundsGizmoColor;
-            Gizmos.DrawWireCube( transform.position + bounds.center, bounds.size);
+            Gizmos.DrawWireCube(transform.position + bounds.center, bounds.size);
         }
 
+        #if UNITY_EDITOR
 
         /// <summary>
-        ///  Checks if the bounds are set. Or else it will warn you and the Component would be useless :D
+        ///     Checks if the bounds are set. Or else it will warn you and the Component would be useless :D
         /// </summary>
         private void OnValidate()
         {
             if (IsNotDefault(bounds)) return;
-            Debug.LogWarning("Don't forget to set the boundsProperty!");
+            Debug.LogWarning("Don't forget to set the boundsProperty! Setting up automatically...");
+            CalculateBounds();
         }
         
+        #endif
+
         private static bool IsNotDefault(Bounds boundsProperty)
         {
             return boundsProperty != default;
         }
-        
-        
+
+
         /// <summary>
-        ///  Calculates the bounds of the sprites or colliders.
+        ///     Calculates the bounds of the sprites or colliders.
         /// </summary>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         public void CalculateBounds()
@@ -72,72 +76,80 @@ namespace Klem.Utils
             Vector3 boundsCenter;
             Vector3 boundsMin;
             Vector3 boundsMax;
+            var position = transform.position;
             switch (boundsType)
             {
-                
                 case BoundsType.Sprites:
-                    
+
                     Debug.Log("Bounds2DComponent:  Calculating bounds for sprites");
-                    
+
+
                     var sprRenderer = GetComponentsInChildren<SpriteRenderer>();
                     if (sprRenderer.Length == 0)
                     {
                         Debug.LogWarning("No sprites found!");
                         return;
                     }
-                    
-                    boundsCenter = sprRenderer.Aggregate(Vector3.zero, (current, t) => current + t.bounds.center);
+
+                    boundsCenter = sprRenderer.Aggregate(
+                        Vector3.zero,
+                        (current, t) =>
+                            current + t.bounds.center - position);
                     boundsCenter /= sprRenderer.Length;
-                    bounds.center = boundsCenter - transform.position;
-                    
+                    bounds.center = boundsCenter;
+
                     boundsMin = sprRenderer[0].bounds.min;
                     boundsMax = sprRenderer[0].bounds.max;
-                    
+
                     foreach (var spr in sprRenderer)
                     {
                         boundsMin = Vector3.Min(boundsMin, spr.bounds.min);
                         boundsMax = Vector3.Max(boundsMax, spr.bounds.max);
                     }
-                    
-                    bounds.SetMinMax(boundsMin, boundsMax);
-                    
+
+                    bounds.min = boundsMin - position;
+                    bounds.max = boundsMax - position;
+
                     Debug.Log("Bounds2DComponent:  Bounds calculated!");
-                    
+
                     break;
-                
+
                 case BoundsType.Colliders:
-                    
+
                     Debug.Log("Bounds2DComponent:  Calculating bounds for colliders");
-                    
+
                     var colliders = GetComponentsInChildren<Collider2D>();
                     if (colliders.Length == 0)
                     {
                         Debug.LogWarning("No colliders found!");
                         return;
                     }
-                    
-                    boundsCenter = colliders.Aggregate(Vector3.zero, (current, t) => current + t.bounds.center);
+
+                    boundsCenter = colliders.Aggregate(
+                        Vector3.zero,
+                        (current, t)
+                            => current + t.bounds.center - position);
                     boundsCenter /= colliders.Length;
-                    bounds.center = boundsCenter - transform.position;
-                    
+                    bounds.center = boundsCenter;
+
                     boundsMin = colliders[0].bounds.min;
                     boundsMax = colliders[0].bounds.max;
-                    
+
                     foreach (var coll in colliders)
                     {
                         boundsMin = Vector3.Min(boundsMin, coll.bounds.min);
                         boundsMax = Vector3.Max(boundsMax, coll.bounds.max);
                     }
-                    
-                    bounds.SetMinMax(boundsMin, boundsMax);
-                    
+
+                    bounds.min = boundsMin - position;
+                    bounds.max = boundsMax - position;
+
                     Debug.Log("Bounds2DComponent:  Bounds calculated!");
-                    
+
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
         }
-        
     }
 }
