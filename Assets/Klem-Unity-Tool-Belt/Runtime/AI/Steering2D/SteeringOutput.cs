a8909159547224e09b93621263b05d631249760f
Clément L <clementlandais@gmail.com>
2023-07-06 13:06:48 +0200

    WIP Steering behaviors

---
+++ Assets/Klem-Unity-Tool-Belt/Runtime/AI/Steering2D/SteeringOutput.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Klem.Runtime.AI.Steering2D
+{
+	public struct SteeringOutput
+	{
+		public Vector2 Linear { get; set; }
+		public float Angular { get; set; }
+	}
+}
